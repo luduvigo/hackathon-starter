325a573c0b8e284bee663252a83779c0a7ca3ee5
luduvigo <paoloantoniorossi@gmail.com>
2015-06-20 12:25:55 +0200

    Add Mongoose

---
+++ db.js
@@ -0,0 +1,6 @@
+var mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/hackathon-starter", function(req, res){
+	console.log("Mongo DB connected")
+	
+	module.exports = mongoose
+})
