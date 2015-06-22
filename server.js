bfd7998c34821ebecd4d68f02d55bdc095cad8a9
luduvigo <paoloantoniorossi@gmail.com>
2015-06-23 01:09:00 +0200

    Fix the post ordering

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", function (req, res){
 
 app.get("/api/posts", function (req, res, next){
 	console.log("Post get")
-	Post.find(function(err, posts){
+	Post.find().sort('-date').exec(function(err, posts){
 		if (err) {return next(err)}	
 		res.json(posts)
 	})
