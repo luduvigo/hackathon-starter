c3444a1697ebbd885e2b010331f98a9d32370814
luduvigo <paoloantoniorossi@gmail.com>
2015-08-10 23:52:15 +0200

    Modularization of gulp files

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,5 @@
+angular.module('app', [])
+
 //Create local app module
 var app = angular.module("app", [])
 
@@ -30,3 +32,30 @@ app.controller("PostsCtrl", function($scope, PostsSvc) {
 			$scope.posts = posts	
 		})
 })	
+angular.module('app')
+    .controller('PostsCtrl', function ($scope, PostsSvc) {
+        $scope.addPost = function () {
+            if($scope.postBody){
+                PostsSvc.create({
+                    username : 'gulp',
+                    body : $scope.postBody
+                }).success(function (post) {
+                    $scope.posts.unshift(post)
+                    $scope.postBody = null         
+                })
+            }
+        }
+        PostsSvc.fetch().success(function (posts) {
+            $scope.posts = posts
+        })         
+    })
+
+angular.module('app')
+    .service('PostsSvc', function ($http){
+        this.fetch = function () {
+            return $http.get('/api/posts')
+        }       
+        this.create = function (post) {
+            return $http.post('/api/posts', post)
+        }
+    })
