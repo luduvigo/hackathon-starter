f3a96b2cc2c6e6e1fb98c7bf681716d2a850f66e
luduvigo <paoloantoniorossi@gmail.com>
2015-09-10 00:11:25 +0200

    First password validation

--- authentication/server.js
+++ authentication/server.js
@@ -1,13 +1,27 @@
 var express = require('express')
 var jwt = require('jwt-simple')
+var _ = require('lodash')
 
 var app = express()
 app.use(require('body-parser').json())
 
+var users = [{username : 'luduvigo', password: 'password'}]
+
 var secretKey = 'supersecretkey'
 
+function findUserByUsername(username) {
+    return _.find(users, {username : username })
+}
+
+function validateUser(user, password) {
+    return user.password = password
+}
+
 app.post('/session', function (req, res){
-    var username = req.body.username
+    var user = findUserByUsername(req.body.username)
+    if(!validateUser(user, req.body.password)){
+        return res.send(401)
+    }
     var token = jwt.encode({username : username}, secretKey)
     res.json(token)
 })
